fb75fc9fc015978bdf655b3c782428d7ca44a1b8
ancamariac <ciuciuancamaria@gmail.com>
2022-08-26 14:25:19 +0300

    Fixed camera settings to follow the selected car

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform cameraTarget;
+    float posX;
+    float posY;
+    float posZ;
+
+    private void Start()
+    {
+        posX = transform.position.x;
+        posY = transform.position.y;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 pos = transform.position;
-        pos.z = cameraTarget.position.z;
+        posZ = cameraTarget.position.z - 4;
+
+        Vector3 pos = new Vector3(posX, posY, posZ);
+
         transform.position = pos;
     }
 }
